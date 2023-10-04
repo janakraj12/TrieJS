04cf55e8c984a7677bcee422e077a4e70940fb92
janakraj12 <146827491+janakraj12@users.noreply.github.com>
2023-10-04 12:44:28 +0545

    uploaded main

---
+++ src/Trie.js
@@ -0,0 +1,52 @@
+class TrieNode {
+    constructor() {
+      this.children = {};
+      this.isEndOfWord = false;
+    }
+  }
+  
+  class Trie {
+    constructor() {
+      this.root = new TrieNode();
+    }
+  
+    insert(word) {
+      let node = this.root;
+      for (let char of word) {
+        if (!node.children[char]) {
+          node.children[char] = new TrieNode();
+        }
+        node = node.children[char];
+      }
+      node.isEndOfWord = true;
+    }
+  
+    search(word) {
+      let node = this.root;
+      for (let char of word) {
+        if (!node.children[char]) {
+          return false;
+        }
+        node = node.children[char];
+      }
+      return node.isEndOfWord;
+    }
+  
+    startsWith(prefix) {
+      let node = this.root;
+      for (let char of prefix) {
+        if (!node.children[char]) {
+          return false;
+        }
+        node = node.children[char];
+      }
+      return true;
+    }
+  }
+  
+  // Example usage:
+  const trie = new Trie();
+  trie.insert("apple");
+  console.log(trie.search("apple")); // true
+  console.log(trie.search("app")); // false
+  console.log(trie.startsWith("app")); // true
